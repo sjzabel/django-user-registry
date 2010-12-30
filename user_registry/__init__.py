136525f649e97ceb05edcb9e21a41d4cc431cfff
Stephen Zabel <szabel@q2software.com>
2010-12-30 17:45:41 -0600

    Just being anal

--- user_registry/__init__.py
+++ user_registry/__init__.py
@@ -1,14 +1,4 @@
-class UserRegistryRegisterError(Exception):
-    ''' There is already a user in the registry '''
-    pass
-
-class UserRegistryUnregisterError(Exception):
-    ''' There is no user in the registry '''
-    pass
-
-class UserRegistryUnregisterWrongUserError(Exception):
-    ''' There is the wrong user in the registry '''
-    pass
+from user_registry.exceptions import UserRegistryRegisterError, UserRegistryUnregisterError, UserRegistryUnregisterWrongUserError
 
 class UserRegistry(object):
     '''
